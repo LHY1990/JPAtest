0e7ed0e5b26a8f9dc6be8a67c91ce2c81d63ebcb
LHY1990 <gnsdud3163@gmail.com>
2022-05-01 12:57:46 +0900

    Feat : persistence의 어노테이션 설명추가(테이블,  컬럼명 연동)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+//@Table able(name="USER") 이 경우 USER 라는 테이블과 연결된다.
 public class Member {
 
-    @Id
-    Long id; //javax.persistence가 중요
-    String name;
+    @Id 
+    private Long id; //javax.persistence가 중요
+    //@Column(name="username") 이 경우 name필드는 username 컬럼과 연동
+    private String name;
 
     public Long getId() {
         return id;
